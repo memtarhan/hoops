9b0fa3770d71e52e2bb7292b48146fd481cbb8e4
Mehmet Tarhan <hi@memtarhan.com>
2025-01-06 11:12:13 +0300

    Create project

---
+++ Hoops/HoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopsApp.swift
+//  Hoops
+//
+//  Created by Mehmet Tarhan on 6.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct HoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
